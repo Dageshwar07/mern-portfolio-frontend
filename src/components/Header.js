be121aa1c9897702b7cac2a709c34c3c98454162
Dageshwar Das <dageshwardasmanikpuri996@gmail.com>
2023-03-06 02:28:12 +0530

    hello

---
+++ src/components/Header.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+
+
+    const common = {
+        marginRight:15,
+        fontSize:17,
+        letterSpacing:".5px"
+    }
+    return (
+        <>
+            <Navbar bg="light" variant="light">
+                <Container>
+                    <div>
+                        <h2 className='mt-2 h2_right' style={{ color: "#6c63ff" }} >
+                            <Nav.Link href="#home">Dageshwar Das</Nav.Link>
+
+                        </h2>
+                    </div>
+                    <Nav className="">
+                    <div className='mt-2'>
+                            <NavLink to="/" className="text-decoration-none" style={common}>Home</NavLink>
+                            <NavLink to="/about" className="text-decoration-none" style={common}>About</NavLink>
+                            <NavLink to="playlist" className="text-decoration-none" style={common}>Projects</NavLink>
+                            <NavLink to="/contact" className="text-decoration-none" style={common}>Contact</NavLink>
+                        </div>
+                        <div>
+                        <Button variant="danger">Github</Button>{' '}
+
+                        </div>
+                    </Nav>
+                </Container>
+            </Navbar>
+        </>
+    )
+}
+
+export default Header
