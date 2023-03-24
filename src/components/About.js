e78eb453a4a7f59b991886dd4936b5f12109c3f5
Dageshwar Das <dageshwardasmanikpuri996@gmail.com>
2023-03-24 19:25:38 +0530

    hii

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ const About = () => {
       <div className="container mb-3 about_container" style={{ minHeight: "100%" }}>
         <div className="container main_container d-flex justify-content-around flex-wrap">
           <div className="left_container mt-5" style={{ width: 500 }}>
-            <h2>Meet Harsh Pathak</h2>
+            <h2>Meet Dageshwar Das</h2>
             <p style={{ color: "#666", letterSpacing: ".5px", marginTop: 2, lineHeight: 2 }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed reiciendis tenetur ex laudantium nam molestiae eius totam assumenda? Ipsa distinctio eum fugit cum adipisci error? Maiores dignissimos amet eaque culpa, recusandae cumque in qui praesentium autem, vero fuga! Doloremque, deserunt.</p>
           </div>
           <div className="right_container mt-3">
